2afbb932a7d3dd734d5dcaf88d27a882b871a095
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2020-08-20 14:53:24 +0200

    Added setup.py for installing

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="bdld",
+    version="0.1.0",
+    description="Simulation algorithm for Langevin dynamics with additional birth/death steps",
+    author="Benjamin Pampel",
+    packages=["bdld"],
+)
