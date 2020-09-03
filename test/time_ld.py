56986ee33e9bc57eb870bc13361ac4deecfb3a20
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2020-09-03 17:07:23 +0200

    LD timing added (not very interesting)

--- test/time_ld.py
+++ test/time_ld.py
@@ -1,15 +1,14 @@
-"""Check the time difference between calculating the full kernel distance matices or each row independently"""
-
+"""Check the time 100 LD steps need for different numbers of particles"""
+from contextlib import redirect_stdout
+import io
 import timeit
+
 import numpy as np
 
 setup = """
 import numpy as np
 from bdld.bussi_parinello_ld import BussiParinelloLD
 from bdld.potential import Potential
-
-# parameters to change
-n_particles = 1000
 
 # set up BussiParinelloLD with double well potential
 pot = Potential(np.array([0, 0, -4, 0, 1]))
@@ -22,8 +21,16 @@ for _ in range(n_particles // 2):
     ld.add_particle([extrema[2]])
 """
 
-time = timeit.Timer(stmt="ld.step()", setup=setup).repeat(10, 100)
-print(
-    f"Time (min, min, max): {min(time)}, {np.mean(time)}, {max(time)}",
-    flush=True,
-)
+n_particles = [10, 100, 1000]
+
+print("Times for 100 LD steps (min, min, max)")
+
+for n in n_particles:
+    with redirect_stdout(io.StringIO()):
+        time = timeit.Timer(
+            stmt="ld.step()", setup="n_particles={}\n".format(n) + setup
+        ).repeat(10, 100)
+    print(
+        f"{n} particles: {min(time)}, {np.mean(time)}, {max(time)}",
+        flush=True,
+    )
