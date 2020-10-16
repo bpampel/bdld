98c5b81fffe2fb26e279119a967965e2bfc4c1c6
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2020-10-16 14:31:32 +0200

    Added convolution between grids
    Also some minor changes/fixes to grid class

--- bdld/grid.py
+++ bdld/grid.py
@@ -1,7 +1,8 @@
 """Custom grid class that holds also the data"""
 
-from typing import List, Union, Tuple
+from typing import Callable, List, Union, Tuple
 import numpy as np
+from scipy import signal
 
 
 class Grid:
@@ -13,7 +14,7 @@ class Grid:
     :param n_points: number of points per dimension
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Create empty, uninitialized class. Should usually not invoked directly"""
         self.data: np.array = None
         self.ranges: List[Tuple[float, float]] = []
@@ -28,10 +29,61 @@ class Grid:
         ]
 
     def points(self) -> np.ndarray:
-        """Return all grid points as a array of shape (n_points, dim)"""
-        return np.array(np.meshgrid(*self.axes())).reshape(
+        """Return all grid points as a array of shape (n_points, dim) in row-major order"""
+        return np.array(np.meshgrid(*self.axes())).T.reshape(
             np.prod(self.n_points), self.n_dim
         )
+
+    def as_array(self) -> np.ndarray:
+        """Return data as structured array in row-major order """
+        return self.data.reshape(self.n_points)
+
+    def set_from_func(self, func: Callable[..., float]) -> None:
+        """Set data by applying function to all points
+
+        The function must accept the input in """
+        self.data = np.array([func(p) for p in self.points()])
+
+    def copy_empty(self):
+        """Get a new grid instance with the same points but without data"""
+        return from_npoints(self.ranges, self.n_points)
+
+
+def convolve(g1: Grid, g2: Grid, mode: str = "valid") -> Grid:
+    """Perform convolution between two grids via scipy.signal.convolve
+
+    Grids must have same dimensions and stepsizes.
+    The convolution is also correctly normalized by the stepsizes.
+
+    :param g1, g2: grids to convolute
+    :param mode: convolution mode, see scipy.signal.convolve for details
+    :return grid: New grid containing the convolutin
+    """
+    if not np.all(g1.stepsizes == g2.stepsizes):
+        raise ValueError("Spacing of grids does not match")
+    stepsizes = g1.stepsizes
+    n_dim = g1.n_dim
+    # to get the same values in the continuous limit: multiply by stepsizes
+    conv = signal.convolve(g1.as_array(), g2.as_array(), mode=mode) * np.prod(g1.stepsizes)
+    # import pdb; pdb.set_trace()
+    # also get the corresponding grid points depending on the method
+    if mode == "same":  # easiest case, mirrors grid of first argument
+        grid = g1.copy_empty()
+    if mode == "valid":  # need to do some math: subtract smaller from larger grid
+        n_points = []
+        ranges = []
+        # sort by points to find smaller grid (conv throws error if not in all dim)
+        gs, gl = sorted([g1, g2], key = lambda g: g.n_points)
+        for dim in range(n_dim):
+            n_points.append(gl.n_points[dim] - gs.n_points[dim] + 1)
+            offset = (gs.n_points[dim] - 1) / 2 * stepsizes[dim]
+            ranges.append((gl.ranges[dim][0] + offset, gl.ranges[dim][1] - offset))
+        grid = from_npoints(ranges, n_points)
+    if mode == "full":
+        raise ValueError("Currently not implemented")
+    grid.data = conv
+    return grid
+
 
 
 def from_npoints(
@@ -79,7 +131,7 @@ def from_stepsizes(
             stepsizes = [stepsizes] * grid.n_dim
     grid.stepsizes = stepsizes
     for i, r in enumerate(ranges):
-        n_points_tmp = int(np.ceil((r[1] - r[0]) / stepsizes[i])) - int(shrink)
+        n_points_tmp = int(np.ceil((r[1] - r[0]) / stepsizes[i])) + 1 - int(shrink)
         r = (r[0], r[0] + stepsizes[i] * n_points_tmp)
         grid.n_points.append(n_points_tmp)
     grid.ranges = ranges
