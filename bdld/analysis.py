Mainly a fix in manual walker density calculation
otherwise lots of style and documentation changes

bdld/analysis.py
@@ -1,5 +1,7 @@
 """Misc analysis functions"""
 
+from typing import List, Tuple
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,7 +12,7 @@ def plot_fes(fes, axes, ref=None, plot_domain=None, filename=None, title=None):
     :param fes: the fes to plot
     :param axes: axes of plot
     :param ref: optional reference FES to plot (makes only sense for 1d fes)
-    :param plot_domain: optional list with minimum and maximum value to show
+    :param plot_domain: optional Tuple with minimum and maximum value to show
     :param filename: optional filename to save figure to
     :param title: optional title for the legend
     """
@@ -95,17 +97,20 @@ def calculate_delta_f(fes, kt, masks):
     return delta_f
 
 
-def count_particles_per_state(particles, ranges):
+def count_particles_per_state(
+    particles: List[np.ndarray], ranges: List[List[Tuple[float, float]]]
+) -> List[int]:
     """Return the number of particles in each state
 
-    currently for 1d only
+    This assumes rectangular states
 
     :param particles: list with all Particles
-    :param ranges: list with [min, max] ranges for each state
+    :param ranges: list with list of [(min_x, max_x), (min_y, max_y), ...] ranges for each state
     """
     counts = [0] * len(ranges)
     for p in particles:
-        for i, ra in enumerate(ranges):
-            if ra[0] < p.pos < ra[1]:
+        for i, state in enumerate(ranges):
+            # check if inside in all dimensions
+            if all([x_min <= p.pos <= x_max for (x_min, x_max) in state]):
                 counts[i] += 1
     return counts

bdld/birth_death.py
@@ -36,7 +36,7 @@ def calc_prob_correction_kernel(eq_density: grid.Grid, bw: np.ndarray) -> grid.G
     log_term = np.log(conv / dens_smaller)
     integral_term = nd_trapz(log_term.data * dens_smaller.data, conv.stepsizes)
     conv = -log_term + integral_term
-    if any(n > 101 for n in conv.n_points):  # make less dense grid to speed up performance
+    if any(n > 101 for n in conv.n_points):  # sparsify grid to speed up performance
         sparse_n_points = [n if n <= 101 else 101 for n in conv.n_points]
         sparse_conv = grid.from_npoints(conv.ranges, sparse_n_points)
         sparse_conv.data = conv.interpolate(sparse_conv.points(), "linear")
@@ -62,6 +62,8 @@ def kernel_sq_dist(dist: np.ndarray, bw: np.ndarray) -> np.ndarray:
 
 def nd_trapz(data: np.ndarray, dx: Union[List[float], float]) -> float:
     """Calculate a multidimensional integral via recursive usage of the trapezoidal rule
+
+    Uses numpy's trapz for the 1d integrals
 
     :param data: values to integrate
     :param dx: distances between datapoints per dimension
@@ -104,13 +106,10 @@ def _walker_density_manual(pos: np.ndarray, bw: np.ndarray) -> np.ndarray:
     :param bw: bandwidth parameter of kernel
     :return density: estimated density at each walker
     """
-    density = np.empty((len(pos)))
-    for i in range(len(pos)):
-        dist = np.fromiter(
-            ((pos[i] - pos[j]) ** 2 for j in range(len(pos)) if j != i),
-            np.float64,
-            (len(pos) - 1, pos.shape[1]),
-        )
+    n_part = len(pos)
+    density = np.empty((n_part))
+    for i in range(n_part):
+        dist = np.array([(pos[i] - pos[j]) ** 2 for j in range(n_part) if j != i])
         kernel_values = kernel_sq_dist(dist, bw)
         density[i] = np.mean(kernel_values)
     return density
@@ -250,8 +249,8 @@ class BirthDeath:
         :return bd_events: list of tuples with particles to duplicate and kill per event
         """
         num_part = len(pos)
-        dup_list = []
-        kill_list = []
+        dup_list: List[int] = []
+        kill_list: List[int] = []
         with np.errstate(divide="ignore"):
             # density can be zero and make beta -inf. Filter when averaging in next step
             beta = np.log(walker_density(pos, self.bw, self.kde)) + ene * self.inv_kt

bdld/bussi_parinello_ld.py
@@ -10,17 +10,16 @@ from bdld.potential import Potential
 class BpldParticle(Particle):
     """Derived Particle class that additionally stores MD related variables
 
-    :param list of float rand: storage for one random number per dimension
-    :param float energy: stores last energy evaluation
-    :param list of float forces: stores last force evaluation
+    :param energy: stores last energy evaluation
+    :param forces: stores last force evaluation per dimension
     :param float c2: constant for the MD thermostat (mass dependent)
     """
 
     def __init__(self, *args) -> None:
         """Creates particle from base class with additional attributes"""
         super().__init__(*args)
-        self.forces: np.array = []
         self.energy: float = 0.0
+        self.forces: np.ndarray = np.empty(0)
         self.c2: float = 0.0  # second thermostat constant depends on mass
 
 

bdld/grid.py
@@ -20,7 +20,7 @@ class Grid:
 
     def __init__(self) -> None:
         """Create empty, uninitialized class. Should usually not invoked directly"""
-        self._data: np.array = np.empty(0)
+        self._data: np.ndarray = np.empty(0)
         self.ranges: List[Tuple[float, float]] = []
         self.n_points: List[int] = []
         self.stepsizes: List[float] = []
@@ -149,7 +149,7 @@ class Grid:
         )
 
     def write_to_file(
-        self, filename: str, fmt: str = "%.18e", header: str = ''
+        self, filename: str, fmt: str = "%.18e", header: str = ""
     ) -> None:
         """Write the grid to file via numpy.savetxt
 
@@ -166,9 +166,9 @@ class Grid:
                 np.c_[self.axes()[0], self.data],
                 fmt=fmt,
                 header=header,
-                delimiter=' ',
-                newline='\n',
-                comments='',
+                delimiter=" ",
+                newline="\n",
+                comments="",
             )
         elif self.n_dim == 2:
             write_2d_sliced_to_file(
@@ -193,12 +193,14 @@ def convolve(g1: Grid, g2: Grid, mode: str = "valid", method: str = "auto") -> G
     :param mode: convolution mode, see scipy.signal.convolve for details
     :return grid: New grid containing the convolutin
     """
-    if not np.all(g1.stepsizes == g2.stepsizes):
+    if not g1.stepsizes == g2.stepsizes:
         raise ValueError("Spacing of grids does not match")
     stepsizes = g1.stepsizes
     n_dim = g1.n_dim
     # to get the same values in the continuous limit: multiply by stepsizes
-    conv = signal.convolve(g1.data, g2.data, mode=mode, method=method) * np.prod(g1.stepsizes)
+    conv = signal.convolve(g1.data, g2.data, mode=mode, method=method) * np.prod(
+        g1.stepsizes
+    )
     # also get the corresponding grid points depending on the method
     if mode == "same":  # easiest case, mirrors grid of first argument
         grid = g1.copy_empty()

bdld/histogram.py
@@ -17,7 +17,7 @@ class Histogram(grid.Grid):
 
     def __init__(
         self,
-        n_bins: Union[np.ndarray, List[int], int],
+        n_bins: Union[List[int], int],
         ranges: List[Tuple[float, float]],
     ):
         """Set up empty histogram instance
@@ -30,7 +30,7 @@ class Histogram(grid.Grid):
         :param fes: the free energy values corresponding to the histogram
         """
         super().__init__()
-        if not isinstance(n_bins, (list, np.ndarray)):  # single float
+        if not isinstance(n_bins, list):  # single float
             n_bins = [n_bins]
         self.n_points = n_bins
         self.ranges = ranges

bdld/potential.py
@@ -66,9 +66,7 @@ class Potential:
         else:
             raise ValueError("No polyval function for more than 3 dimensions")
 
-    def evaluate(
-        self, pos: Union[List[float], np.ndarray]
-    ) -> Tuple[np.ndarray, np.ndarray]:
+    def evaluate(self, pos: Union[List[float], np.ndarray]) -> Tuple[float, np.ndarray]:
         """Get potential energy and forces at position
 
         :param pos: position to be evaluated
@@ -86,7 +84,7 @@ class Potential:
         return self.polyval(*pos, self.coeffs)
 
     def calculate_reference(
-        self, pos: Union[List[float], np.ndarray], mintozero: bool = True
+        self, pos: Union[List[np.ndarray], np.ndarray], mintozero: bool = True
     ) -> np.ndarray:
         """Calculate reference from potential at given positions
 