6d4e41394ff8a6b4a6850fe9c2aea82fdcfb14e3
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2021-01-19 19:28:03 +0100

    Update test to new package structure

--- test/test_potential.py
+++ test/test_potential.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from bdld import potential
+from bdld.potential import polynomial
 
 
 class PotentialTests(unittest.TestCase):
@@ -13,17 +13,17 @@ class PotentialTests(unittest.TestCase):
 
     def test_create(self):
         """Initialization of Potential"""
-        pot = potential.Potential(self.c1)
+        pot = polynomial.PolynomialPotential(self.c1)
         np.testing.assert_array_equal(pot.coeffs, self.c1)
         np.testing.assert_array_equal(pot.der[0], [0, 2])
         self.assertFalse(pot.ranges)
         self.assertEqual(f"{pot}", "polynomial with coefficients [0 0 1]")
         with self.assertRaises(ValueError):  # more than 3 dim
-            pot = potential.Potential(np.ones((1, 1, 1, 4)))
+            pot = polynomial.PolynomialPotential(np.ones((1, 1, 1, 4)))
 
     def test_evaluate(self):
         """Test if correct values are returned"""
-        pot = potential.Potential(self.c2)
+        pot = polynomial.PolynomialPotential(self.c2)
         testpos = [0.5, 2.0]
         energy, forces = pot.evaluate(testpos)
         self.assertEqual(energy, 8.5)  # x+2*y**2
@@ -31,14 +31,14 @@ class PotentialTests(unittest.TestCase):
 
     def test_reference(self):
         """Test reference function"""
-        pot = potential.Potential(self.c1)
+        pot = polynomial.PolynomialPotential(self.c1)
         testpos = np.linspace(0, 5, 6).reshape(6, 1)
         fes = pot.calculate_reference(testpos)
         np.testing.assert_array_equal(fes, [0, 1, 4, 9, 16, 25])
 
     def test_probability_density(self):
         """Test if probabilities are correct"""
-        pot = potential.Potential([0, 0, 1])
+        pot = polynomial.PolynomialPotential([0, 0, 1])
         prob = pot.calculate_probability_density(0.5, [(-1, 1)], [3])
         expect = np.array([np.exp(-2), 1, np.exp(-2)])  # not yet normalized
         np.testing.assert_array_equal(prob.data, expect / np.sum(expect))
