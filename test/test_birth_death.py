246ccab7290ee64975a35d3322cc4ac65438e412
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2021-02-25 14:10:45 +0100

    Birth death: testing for different density estimation methods added

--- test/test_birth_death.py
+++ test/test_birth_death.py
@@ -10,13 +10,13 @@ class BirthDeathTests(unittest.TestCase):
 
     def test_kernel_1d(self):
         """Test the gaussian kernel in 1d"""
-        bw = np.array([1])
+        bw = np.array([2])
         center = np.array([[0]])
-        height = 1 / np.sqrt(2 * np.pi)  # expected value
+        height = 1 / (np.sqrt(2 * np.pi) * 2)  # expected value
         self.assertEqual(bd.kernel_sq_dist(center, bw), height)
 
         testdist = np.array([[1]])
-        self.assertEqual(bd.kernel_sq_dist(testdist, bw), height * np.exp(-0.5))
+        self.assertEqual(bd.kernel_sq_dist(testdist, bw), height * np.exp(-1/8))
 
     def test_kernel_2d(self):
         """Test the gaussian kernel in 2d"""
@@ -29,6 +29,49 @@ class BirthDeathTests(unittest.TestCase):
         self.assertEqual(
             bd.kernel_sq_dist(testdist, bw), height * np.exp(-0.5) * np.exp(-0.25)
         )
+
+    def test_walker_density(self):
+        """Test evaluation of walker density with all 3 different methods"""
+        bw = np.array([0.5])
+        positions = np.array([[0],[0.5],[1]])
+        dens_pdist = bd.walker_density(positions, bw)  # chooses pdist variant
+        dens_kde = bd.walker_density(positions, bw, kde=True)
+        dens_manual = bd._walker_density_manual(positions, bw)
+
+        # expected: (kernel values from Wolfram alpha "N(0,0.25) at x=0 / 0.5 / x=1")
+        kernel_values = [0.797885, 0.483941, 0.107982]  # 0, 0.5, 1 distance
+        expected = np.empty(3)  # mean of kernel values (distance to positions)
+        expected[0] = np.mean(kernel_values)
+        expected[1] = np.mean([kernel_values[0]] + 2*[kernel_values[1]])
+        expected[2] = expected[0]  # reversed positions -> same density as first
+
+        np.testing.assert_allclose(dens_pdist, expected, rtol=1e-6)
+        np.testing.assert_allclose(dens_kde, expected, rtol=1e-6)
+        np.testing.assert_allclose(dens_manual, expected, rtol=1e-6)
+
+    def test_walker_density_2d(self):
+        """Test evaluation of walker density in 2d"""
+        bw = np.array([0.5, 2])
+        positions = np.array([[0,0],[0.5,-1],[1,-2]])
+        dens_pdist = bd.walker_density(positions, bw)  # chooses pdist variant
+        dens_kde = bd.walker_density(positions, bw, kde=True)
+        dens_manual = bd._walker_density_manual(positions, bw)
+
+        # expected: (kernel values from Wolfram alpha "N(0,0.25) at x=0 / 0.5 / x=1")
+        kernel_values = np.zeros((3,2))
+        kernel_values[:,0] = np.array([0.797885, 0.483941, 0.107982])
+        kernel_values[:,1] = np.array([0.19947114020, 0.17603266338, 0.12098536226])
+
+        expected = np.empty(3)  # mean of kernel values (distance to positions)
+        expected[0] = np.mean(np.prod(kernel_values, axis=1))
+        kernel_pos1 = np.array([kernel_values[1], kernel_values[0], kernel_values[1]]) # for particle 1
+        expected[1] = np.mean(np.prod(kernel_pos1, axis=1))
+        expected[2] = expected[0]  # reversed positions -> same density as first
+
+        np.testing.assert_allclose(dens_pdist, expected, rtol=1e-6)
+        np.testing.assert_allclose(dens_kde, expected, rtol=1e-6)
+        np.testing.assert_allclose(dens_manual, expected, rtol=1e-6)
+
 
     def test_kernel_convolution(self):
         """Test the convolution with the kernel needed for the correction"""
