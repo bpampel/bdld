7d5c5f23fbf3696b6d51a9da0f9953501aacba9e
Benjamin Pampel <pampel@mpip-mainz.mpg.de>
2022-09-14 09:56:36 +0200

    Comment code

--- bdld/potential/__init__.py
+++ bdld/potential/__init__.py
@@ -1,5 +1,8 @@
+"""Import all submodules"""
+# when doing `import bdld.potential`
 from . import mueller_brown
 from . import polynomial
 from . import potential
 
+# when doing `from bdld.potential import *` (not recommended)
 __all__ = ["mueller_brown", "polynomial", "potential"]
